e56dbfe396b662ffc58a07e7a07c4e5be008ce24
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2021-05-04 22:22:59 +0900

    register.vue作成・レビュー依頼

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,45 +1,110 @@
 <template>
-   <div id="overlay">
-        <div id="content">
-            <div class="title">
-                <span>新規登録</span>
-            </div>
-            <div class="checkItems">
-                <input type="date" v-model="deadline"/>
-                <input type="color" />
-                <select></select>
-            </div>
-            <div class="todoTextArea">
-                <textarea class="todoText" cols="30" rows="10" placeholder="予定を入力してください"></textarea>
-            </div>
-            <div class="textLeft">
-                <button type="button" @click="$emit('close')" class="btn btn-success">閉じる <i class="fas fa-times"></i></button>
-                <button type="button" @click="register" class="btn btn-success">保存する <i class="fas fa-times"></i></button>
-           </div>
-       </div>
-   </div>
+  <div id="content">
+    <div class="title">
+      <span>新規登録</span>
+    </div>
+    <hr />
+    <nav>
+      <input class="datePic" type="date" v-model="deadline" />
+      <color-list class="colorFilter" @changeColorCode="changeColorCode" />
+    </nav>
+    <div class="todoTextArea">
+      <textarea v-model="todoContent" class="todoText" placeholder="予定を入力してください"></textarea>
+    </div>
+    <div class="textLeft">
+      <button type="button" @click="backTop" class="btnBack btn-success">戻る<i class="fas fa-times"></i></button>
+      <button type="button" :disabled="validate" @click="regist" class="btn btn-success">
+        登録する <i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ColorList from '../components/ColorList.vue'
+import axios from 'axios'
+
+// pthonのapiを使用するためのベースとなるURLをグローバル変数で初期化
+const apiUrl = process.env.VUE_APP_API_URL
 
 export default defineComponent({
-    mounted: function() {
-        let today = new Date();
-        let year = today.getFullYear();
-        let month = today.getMonth();
-        let day = today.getDate();
-
-    },       
-    data() {
-        return {
-            deadline: Date
-        }
-    } 
+  components: {
+    ColorList,
+  },
+  computed: {
+    validate: function() {
+      return !this.validateDeadline && !this.validateColor && !this.validateTodoContent ? false : true
+    },
+    validateDeadline() {
+      return !this.deadline
+    },
+    validateColor: function() {
+      return !this.colorCode
+    },
+    validateTodoContent: function() {
+      return !this.todoContent
+    },
+  },
+  data() {
+    return {
+      deadline: null,
+      todoContent: null,
+      todo: {},
+      colorCode: null,
+    }
+  },
+  methods: {
+    // 子コンポーネントのカラーコードを取得する
+    changeColorCode(colorCode: any) {
+      this.colorCode = colorCode
+    },
+    backTop() {
+      // 登録を破棄するかどうか確認する
+      const result = confirm('変更を破棄してトップに戻りますか？')
+      if (result) {
+        // 確認してOKの場合、一覧のトップページへ遷移する
+        this.$router.push('/')
+      }
+    },
+    regist() {
+      let self = this
+      const todo = {
+        content: this.todoContent,
+        deadline: this.deadline,
+        colorCode: this.colorCode,
+      }
+      // axiosでserver側にとばしてtodoを登録する処理を記述する
+      axios.post(`${apiUrl}/regist`, todo).then(function() {
+        self.$router.push('/')
+      })
+    },
+  },
 })
 </script>
 
-
 <style lang="scss" scoped>
-
+#content {
+  max-width: 760px;
+  min-width: 330px;
+  padding: 0 10px;
+  margin: 0 auto;
+  nav {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    .datePic {
+      margin-right: 10px;
+      height: 30px;
+    }
+  }
+  .todoText {
+    width: 70%;
+    border-radius: 4px;
+    height: 50%;
+  }
+  .btnBack {
+    margin-right: 10px;
+  }
+}
 </style>
